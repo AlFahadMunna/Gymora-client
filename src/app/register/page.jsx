67ceb1a363d5389409da775a5196a70e35e9bd70
AlFahadMunna <afmunna.afm.afm@gmail.com>
2026-06-20 23:17:18 +0600

    registration form completed

---
+++ src/app/register/page.jsx
@@ -0,0 +1,15 @@
+import RegisterForm from "@/components/auth /RegisterForm";
+import React from "react";
+
+export const metadata = {
+  title: "Register - Gymora",
+};
+const Register = () => {
+  return (
+    <div>
+      <RegisterForm></RegisterForm>
+    </div>
+  );
+};
+
+export default Register;
