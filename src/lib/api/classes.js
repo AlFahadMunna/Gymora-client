3c5e0880a3b40f6605225b3a556d2434cad457ca
AlFahadMunna <afmunna.afm.afm@gmail.com>
2026-07-29 21:20:01 +0600

    get approved class

--- src/lib/api/classes.js
+++ src/lib/api/classes.js
@@ -1,4 +1,7 @@
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
- export const getTrainerClasses = async (trainerId)=>{
-    const res = await fetch(`${baseUrl}/api/classes?classId=${trainerId}`)
- }
+export const getTrainerClasses = async (trainerId, status = "approved") => {
+  const res = await fetch(
+    `${baseUrl}/api/classes?classId=${trainerId}$status=${status}`,
+  );
+  return res.json();
+};
